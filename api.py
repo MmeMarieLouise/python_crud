1a079d0c0056d6a67d9566466b1feca9518a196b
Marie-Louise <m.annan@wellcome.ac.uk>
2019-05-14 13:35:53 +0100

    wip failing test

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +9,10 @@ def index():
 
 @app.route('/create', methods = ['POST'])
 def create():
+    received_data = request.get_json()
+    # get data from request
+    # myFile = open("new", "w+")
+    # write hello to test-file
     return 'file created'
 
 
--- api_test.py
+++ api_test.py
@@ -10,11 +10,16 @@ class ApiTest(unittest.TestCase):
 
 
     def test_post_create(self):
-        r = requests.post("http://127.0.0.1:5000/create")
+        data = {'path': '~/crud', 'name': 'test-file', 'contents':'hello'}
+        r = requests.post("http://127.0.0.1:5000/create", data=data)
         code = r.status_code
         text = r.text
         self.assertEqual(text, "file created")
         self.assertEqual(code, 200)
+        file_object = open("~/crud/test-file", "r")
+        # ensure file is there
+        # ensure file has correct contents
+
 
 
 if __name__ == '__main__':
