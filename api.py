6aaad0626424cf5a075e363c5d7b665c8ef12a3f
Marie-Louise Annan <ml_annan@hotmail.com>
2019-05-28 11:49:10 +0100

    add notes & comments

--- api.py
+++ api.py
@@ -1,23 +1,39 @@
+# import the Flask (capitalised) class
 from flask import Flask
+# import request - global object
 from flask import request
 
+# create the class instance , app / instantiate the class
+# pass in 'dunder' (double underscore) name, this is the namespace variable
+# use whatever the current namespace is
 app = Flask(__name__)
 
+# route decorator (function) creates a view
+# specify the route with ('/')
 @app.route('/')
+# index function defines the route
 def index():
     return ''
 
+
+# there are multiple routes to allow the app to respond on multiple end points
+# make a request using HTTP 'POST' method to create file at specified path
 @app.route('/create', methods=['POST'])
 def create():
+    # name response object 'received_data', store JSON within it
     received_data = request.get_json()
+    #
     path_key = received_data['path']
     name_key = received_data['name']
+    # file method 'open()'
     myFile = open( path_key + "/" + name_key, "w+")
+    # file method 'write()'
     myFile.write(received_data['contents'])
+    # file method 'close()'
     myFile.close()
     return 'file created'
 
-
+# make a request using HTTP 'GET' method to read file at specified path
 @app.route('/read', methods=['GET'])
 def read():
     received_data = request.get_json()
@@ -28,4 +44,6 @@ def read():
     file_object.close()
     return read_content
 
+# run the app
+# debug = true , so that Flask will auto re-start anytime changes are made
 app.run(debug=True)
---
+++ notes.md
@@ -0,0 +1,20 @@
+# Notes
+
+
+- why is namespace `__name__ __main__` created  ?
+- `w+` ?
+- `r` ?
+- http://127.0.0.1:5000 ?
+- how to navigate to `bin` dir?
+
+### Tips
+
+##### Vim commands
+
+`i` - insert
+
+`yy` - yank (copy)
+
+`p` - paste
+
+`dd` - delete current line
